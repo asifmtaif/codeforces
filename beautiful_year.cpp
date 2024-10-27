15824634f858f445bf9eb5dfdb601825a9f24aba
Md. Asif Mahmud Taif <159027480+asifmtaif@users.noreply.github.com>
2024-10-27 21:48:09 +0600

    Create beautiful_year.cpp
    codeforece_800

---
+++ beautiful_year.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>> n;
+    
+    for(;;){
+        n++;
+        int a= n%10;
+        int b= (n/10)%10;
+        int c= (n/100)%10;
+        int d= (n/1000)%10;
+        if(a!=b && a!=c && a!=d && b!=c && b!=d && c!=d){
+            cout<<n;
+            break;
+        }
+    }
+
+    return 0;
+}
