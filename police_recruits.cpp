3239fd94a640cecb5c594d4e32516c3ca44715f7
Md. Asif Mahmud Taif <159027480+asifmtaif@users.noreply.github.com>
2024-11-03 23:38:53 +0600

    Create police_recruits.cpp

---
+++ police_recruits.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n , p=0, c=0;
+    cin>> n;
+    for (int i=0; i<n; i++){
+        int x;
+        cin>>x;
+        if (x>0){
+            p+=x;
+        }
+        else{
+            if (p<1){
+                c++;
+            }
+            else {
+                p--;
+            }
+        }
+    }
+    cout<<c;
+
+    return 0;
+}
